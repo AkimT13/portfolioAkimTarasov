3a1ab6225c693a0d5614d89f5047f613ab76a1f6
AkimT13 <akimtarasov@gmail.com>
2024-01-23 09:53:56 -0800

    projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Project = ({ title, description, use, tech, img }) => {
-  return <div className="w-32 h-52 border rounded-2xl "></div>;
+  return (
+    <div className="w-[20rem] h-auto border rounded-2xl bg-zinc-900 hover:scale-105 transition ease-in-out duration-500">
+      <img src={img} />
+      <hr />
+      <p className="text-white p-3 text-md font-VarelaRound">Title: {title}</p>
+      <p className="text-white p-3 text-md font-VarelaRound">
+        {" "}
+        Description: {description}
+      </p>
+    </div>
+  );
 };
 
 export default Project;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-
+import Project from "./Project";
 const Projects = () => {
   return (
     <div>
       <p className="text-white text-3xl p-10 md:pl-36 font-VarelaRound">
         Projects
       </p>
-      <div className="flex"></div>
+      <div className="flex justify-center gap-5">
+        <Project
+          img="/public/logopupleBluewithtext.png"
+          title="SFHacks 2024"
+          tech="Astro, React, Firebase, JavaScript, Tailwind"
+          description="The website for the SFHacks Hackathon.  "
+        />
+        <Project />
+      </div>
     </div>
   );
 };
